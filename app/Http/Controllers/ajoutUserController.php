195719083a4591846ac553493786046526704eaf
EstebanBELFIS <ebelfis@gmail.com>
2019-12-16 16:44:17 +0100

    Error/Info Debug

--- app/Http/Controllers/ajoutUserController.php
+++ app/Http/Controllers/ajoutUserController.php
@@ -72,10 +72,10 @@ class ajoutUserController extends Controller
             $info = $gsbFrais->ajoutInfos($idVisiteur, $nom, $prénom, $cp, $ville, $adresse, $email, $tel, $embauche,$password);
             $travail = $gsbFrais->ajoutTravail($idVisiteur, $role, $reg);
             //Confirmer l'ajout
-            return view('confirmAjoutUser', compact('info', 'error'));
+            return view('confirmAjoutUser');
         }
         else{
-            return view('RejetUser', compact('info', 'error'));
+            return view('RejetUser');
         }
         
         
